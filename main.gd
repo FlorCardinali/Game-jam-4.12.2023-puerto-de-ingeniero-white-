9de5e43a88709d379aab9bb3ba3fe6049bbca108
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2023-04-16 20:27:14 -0300

    movimiento del barco
    hasta ahi se mueve

--- main.gd
+++ main.gd
@@ -8,6 +8,7 @@ var nivel = 1
 var barco_instancia
 var hay_barco = false
 var vector_barco = Vector2(10,0)
+var vector_caja = Vector2(11,0)
 var colision_barco
  #caja
 var caja_instancia
@@ -20,19 +21,20 @@ func _process(delta):
 		barco_instancia = barco.instantiate()
 		caja_instancia = caja.instantiate()
 		add_child(barco_instancia)
-		barco_instancia.position = Vector2(-1000,750)
+		barco_instancia.position = Vector2(-800,750)
 		add_child(caja_instancia)
-		caja_instancia.position = Vector2(20,400)
+		caja_instancia.position = Vector2(20,780)
 		hay_barco = true
 	elif hay_barco == true and $timer_barco.timeout:
-		recorrer_mapa(barco_instancia)
+		recorrer_mapa(barco_instancia, caja_instancia)
 
-func recorrer_mapa(barco):
-	colision_barco = barco_instancia.move_and_collide(vector_barco)
-	print(barco_instancia.position)
-	if barco_instancia.position.x == 2000:
-		barco_instancia.queue_free()
+func recorrer_mapa(barco, caja):
+	colision_barco = barco.move_and_collide(vector_barco)
+	print(barco.position)
+	if barco.position.x >= 2000:
+		barco.queue_free()
+		caja.queue_free()
 		hay_barco= false
 	if colision_barco!=null:
-		if colision_barco.get_collider().name == "caja":
-			caja_instancia.empujar(vector_barco)
+		if colision_barco.get_collider().is_in_group("cajas"):
+			caja.empujar(vector_caja)
