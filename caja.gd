11a49ee99131060a778fe9988ec1af162c088876
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2023-04-19 15:55:57 -0300

    se mueve el camion

--- caja.gd
+++ caja.gd
@@ -1,16 +1,17 @@
 extends CharacterBody2D
 
-
-var esta_adentro = false
+var click_esta_adentro = false
 var distancia_al_click = Vector2.ZERO
 var colision
-	
+var se_solto_caja = false
+
+
 func _physics_process(delta):
-	if esta_adentro:
-		print("mask: ", get_collision_mask())
+	if click_esta_adentro:
+#		print("mask: ", get_collision_mask())
 		#la caja empiesa en 1 y lo seteas a 2 cuando agarras la caja
 		collision_layer = 2
-		print("layer: ", get_collision_layer())
+#		print("layer: ", get_collision_layer())
 		colision = move_and_collide(get_global_mouse_position() - distancia_al_click - position)
 
 func empujar(velocidad):
@@ -19,8 +20,11 @@ func empujar(velocidad):
 func _on_area_click_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("click_izquierdo"):
 		distancia_al_click = get_global_mouse_position() - self.position
-		esta_adentro = true
+		click_esta_adentro = true
 		print("se clikio")
 	if event.is_action_released("click_izquierdo"):
-		esta_adentro = false
+		click_esta_adentro = false
+		se_solto_caja = true
 		print("se solto el click")
+
+ 
--- main.gd
+++ main.gd
@@ -1,6 +1,8 @@
 extends Node
+
 @export var barco:PackedScene 
 @export var caja:PackedScene
+@export var camion:PackedScene
 
 var nivel = 1
 
@@ -12,24 +14,53 @@ var vector_caja = Vector2(11,0)
 var colision_barco
  #caja
 var caja_instancia
+#camion
+var camion_instancia
+var hay_camion = false
+var colision_camion
+var vector_camion = Vector2(-5,0)
 
 func _ready():
 	pass
 	
 func _process(delta):
+	if not hay_camion:
+		camion_instancia = camion.instantiate()
+		add_child(camion_instancia)
+		#arranca en 2300
+		camion_instancia.position = Vector2(2300,250)
+		hay_camion = true
+	else:
+		recorrer_mapa_camion(camion_instancia)
+		
 	if not hay_barco:
 		barco_instancia = barco.instantiate()
-		caja_instancia = caja.instantiate()
 		add_child(barco_instancia)
 		barco_instancia.position = Vector2(-800,750)
+		
+		caja_instancia = caja.instantiate()
 		add_child(caja_instancia)
 		caja_instancia.position = Vector2(-350,780)
+		
 		hay_barco = true
 	elif hay_barco and $timer_barco.timeout:
-		recorrer_mapa(barco_instancia, caja_instancia)
+		recorrer_mapa_barco(barco_instancia, caja_instancia)
 
 
-func recorrer_mapa(barco, caja):
+func recorrer_mapa_camion(camion):
+	print("recorrer mapa camion")
+	print(camion.position)
+	colision_camion = camion.move_and_collide(vector_camion)
+	#se va en -500
+	if camion.position.x <= -500:
+		camion.queue_free()
+		hay_camion = false
+
+
+
+
+
+func recorrer_mapa_barco(barco, caja):
 	colision_barco = barco.move_and_collide(vector_barco)
 #	print(barco.position)
 	if barco.position.x >= 2500:
@@ -40,5 +71,6 @@ func recorrer_mapa(barco, caja):
 		if colision_barco.get_collider().is_in_group("cajas") and caja.position.x <= 1930:
 			caja.empujar(vector_caja)
 		elif caja.position.x >= 1930:
-			caja.queue_free()
+			caja.position = Vector2(2500,2500)
 	
+
