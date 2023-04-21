be1707a89c03a0e6a93e8c950d9475ff5966ad9c
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2023-04-21 18:07:02 -0300

    comentarios

--- main.gd
+++ main.gd
@@ -14,7 +14,7 @@ var hay_barco = false
 var vector_barco = Vector2(10,0)
 var vector_caja = Vector2(11,0)
 var colision_barco
- #caja
+#caja
 var caja_instancia
 #camion
 var camion_instancia #donde se guarda la copia del camion
@@ -29,26 +29,24 @@ func _ready():
 	
 
 func cuando_se_suelta_la_caja(cajita):
+	#control de posicionamiento de cajas en areas de descanso
 	if  $area_1.get_overlapping_bodies().size() > 0:
-#		print("area 1: ",$area_1.get_overlapping_bodies())
 		if $area_1.get_overlapping_bodies().size() > 1:
 			$area_1.get_overlapping_bodies()[1].queue_free()
 		$area_1.get_overlapping_bodies()[0].position = $area_1.position
 	
 	if $area_2.get_overlapping_bodies().size() > 0:
-#		print("area 2:", $area_2.get_overlapping_bodies())
 		if $area_2.get_overlapping_bodies().size() > 1:
 			$area_2.get_overlapping_bodies()[1].queue_free()
 		$area_2.get_overlapping_bodies()[0].position = $area_2.position
 	
 	if $area_3.get_overlapping_bodies().size() > 0:
-#		print("area 3: ", $area_3.get_overlapping_bodies())
 		if $area_3.get_overlapping_bodies().size() > 1:
 			$area_3.get_overlapping_bodies()[1].queue_free()
 		$area_3.get_overlapping_bodies()[0].position = $area_3.position
 	
 	
-	
+	#control de eliminacion de caja por mala posicion
 	if (cajita.position != $area_1.position) and (cajita.position != $area_2.position) and (cajita.position != $area_3.position and (cajita.position != area_camioncito.global_position)):
 		print("cajita no esta donde debe estar")
 		$explocion.stop()
@@ -60,6 +58,7 @@ func cuando_se_suelta_la_caja(cajita):
 
 func _process(delta):
 	
+	#creador de camiones
 	if not hay_camion:
 		camion_instancia = camion.instantiate()
 		add_child(camion_instancia)
@@ -68,7 +67,8 @@ func _process(delta):
 
 	if hay_camion and $Timer_general.timeout:
 		recorrer_mapa_camion(camion_instancia)
-	
+		
+	#creador de barcos y cajas
 	if not hay_barco:
 		barco_instancia = barco.instantiate()
 		add_child(barco_instancia)
@@ -86,18 +86,22 @@ func _process(delta):
 
 
 func recorrer_mapa_camion(par_camion):
+	
 	colision_camion = par_camion.move_and_collide(vector_camion)
+	
+	#detector de cajas camion
 	area_camioncito = par_camion.get_node("area_camion")
 	if area_camioncito.get_overlapping_bodies().size() == 1:
-#		if area_camioncito.get_overlapping_bodies()[0].name == "caja" or area_camioncito.get_overlapping_bodies()[0].name == ("@caja@"+ str(cant_cajas)):
 		area_camioncito.get_overlapping_bodies()[0].position = area_camioncito.global_position
 	elif area_camioncito.get_overlapping_bodies().size() > 1:
 		#problema, si hay mas de un objeto en el arreglo
 		print("hay ams de unaaaaaaaaa")
-	
+		
+	#eliminacion de camion y caja por izquierda
 	if par_camion.position.x <= -400:
 		if area_camioncito.get_overlapping_bodies().size() > 0:
 			area_camioncito.get_overlapping_bodies()[0].queue_free() #eliminacion de caja
+		#contado de puntos
 			puntos += 1
 			print(puntos)
 		par_camion.queue_free()
@@ -105,19 +109,22 @@ func recorrer_mapa_camion(par_camion):
 
 
 func recorrer_mapa_barco(barco, caja):
+	
 	colision_barco = barco.move_and_collide(vector_barco)
-#	print(barco.position)
+	
+	#eliminacion del barco
 	if barco.position.x >= 2500:
 		barco.queue_free()
 		hay_barco= false
-
+	#control de seguimiento caja a barco
 	if colision_barco!=null:
 		if colision_barco.get_collider().is_in_group("cajas") and caja.position.x <= 1930:
 			caja.empujar(vector_caja)
+		#eliminacion de la caja por derecha
 		elif caja.position.x >= 1930:
 			caja.queue_free()
 
-
+#animacion de explocion
 func _on_explocion_animation_finished():
 	$explocion.stop()
 	$explocion.visible = false
