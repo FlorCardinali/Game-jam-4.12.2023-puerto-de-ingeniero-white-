1f59cb5e51b38ba64357eb1f6a3c7f5152931055
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2023-04-21 03:36:29 -0300

    caja se elimina correctamente
    la caja se elimina si esta fuera de las areas y tambien si hay mas de una caja en cada area

--- caja.gd
+++ caja.gd
@@ -4,7 +4,9 @@ var click_esta_adentro = false
 var distancia_al_click = Vector2.ZERO
 var colision
 var se_solto_caja = false
-signal se_solto_la_caja
+var areas
+var pos_vieja
+signal se_solto_la_caja(caja)
 
 func _physics_process(delta):
 	if click_esta_adentro:
@@ -18,11 +20,12 @@ func empujar(velocidad):
 func _on_area_click_input_event(viewport, event, shape_idx):
 	if event.is_action_pressed("click_izquierdo"):
 		distancia_al_click = get_global_mouse_position() - self.position
+		pos_vieja = position
 		click_esta_adentro = true
 		print("se clikio")
 	if event.is_action_released("click_izquierdo"):
-		click_esta_adentro = false
-		emit_signal("se_solto_la_caja")
+		emit_signal("se_solto_la_caja",self)
+		click_esta_adentro = false #soltado
 		print("se solto el click")
 
  
--- main.gd
+++ main.gd
@@ -4,6 +4,7 @@ extends Node
 @export var caja:PackedScene
 @export var camion:PackedScene
 
+var escena_caja = load("res://caja.tscn")
 var nivel = 1
 
 #barco
@@ -23,17 +24,41 @@ var hay_camion = false #lo que controla que haya un solo camion
 var colision_camion #donde se guarda el movimiento del camion
 var vector_camion = Vector2(-10,0) #velocidad a la que va el camion
 
+
 func _ready():
 	pass
+
+func cuando_se_suelta_la_caja(cajita):
+	if  $area_1.get_overlapping_bodies().size() > 0:
+#		print("area 1: ",$area_1.get_overlapping_bodies())
+		if $area_1.get_overlapping_bodies().size() > 1:
+			$area_1.get_overlapping_bodies()[1].queue_free()
+		$area_1.get_overlapping_bodies()[0].position = $area_1.position
 	
+	if $area_2.get_overlapping_bodies().size() > 0:
+#		print("area 2:", $area_2.get_overlapping_bodies())
+		if $area_2.get_overlapping_bodies().size() > 1:
+			$area_2.get_overlapping_bodies()[1].queue_free()
+		$area_2.get_overlapping_bodies()[0].position = $area_2.position
+	
+	if $area_3.get_overlapping_bodies().size() > 0:
+#		print("area 3: ", $area_3.get_overlapping_bodies())
+		if $area_3.get_overlapping_bodies().size() > 1:
+			$area_3.get_overlapping_bodies()[1].queue_free()
+		$area_3.get_overlapping_bodies()[0].position = $area_3.position
+	
+	if (cajita.position != $area_1.position) and (cajita.position != $area_2.position) and (cajita.position != $area_3.position):
+		print("cajita no esta donde debe estar")
+		cajita.queue_free()
+	
+
 func _process(delta):
 	if not hay_camion:
 		camion_instancia = camion.instantiate()
 		add_child(camion_instancia)
-		print("hayu uncamion anda a saber donde")
 		camion_instancia.position = Vector2(2300,250)
-		print(camion_instancia.position)
 		hay_camion = true
+
 	if hay_camion and $Timer_general.timeout:
 		recorrer_mapa_camion(camion_instancia)
 	
@@ -45,9 +70,11 @@ func _process(delta):
 		caja_instancia = caja.instantiate()
 		add_child(caja_instancia)
 		caja_instancia.position = Vector2(-350,780)
+		caja_instancia.connect("se_solto_la_caja", cuando_se_suelta_la_caja)
+		
 		
 		hay_barco = true
-		
+
 	if hay_barco and $Timer_general.timeout:
 		recorrer_mapa_barco(barco_instancia, caja_instancia)
 
@@ -73,15 +100,3 @@ func recorrer_mapa_barco(barco, caja):
 			caja.empujar(vector_caja)
 		elif caja.position.x >= 1930:
 			caja.position = Vector2(2500,2500)
-
-
-
-
-
-func _on_area_1_area_entered(area):
-	hay_caja_en_1=true
-	print("entro en 1")
-
-func _on_area_1_area_exited(area):
-	hay_caja_en_1= false
-	print("salio de 1")
